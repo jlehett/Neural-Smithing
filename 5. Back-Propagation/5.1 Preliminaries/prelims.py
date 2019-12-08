b88ca6fe5413804085f2ef07b772b0e2c2481810
jlehett <jlehett117@knights.ucf.edu>
2019-12-08 18:20:31 -0500

    Added a fixed Section 5.1

---
+++ 5. Back-Propagation/5.1 Preliminaries/prelims.py
@@ -0,0 +1,73 @@
+"""
+    EXTERNAL LIBRARIES
+"""
+
+import sys
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import math
+
+
+"""
+    INTERNAL UTIL LIBRARIES
+"""
+
+# Add parent directory to the sys path
+sys.path.append('../')
+# Import any python modules from parent directory
+from utils.auxfunctions import MultiLayerNetwork
+from utils.auxfunctions import sigmoid
+
+
+"""
+    In the forward pass, the network computes an output based on its
+    current inputs. Each node, i, computes a weighted sum Ai of its
+    inputs and passes this through a nonlinearity to obtain the node
+    output Yi.
+"""
+
+# Create a subclass that adds a feedforward function to the
+# basic mult-layer network class setup in the auxfunctions module
+class FeedforwardFunctionNetwork(MultiLayerNetwork):
+    
+    def feedforward(self, inputs, activationFunction):
+        # Fill the input nodes
+        if self.bias:
+            for input_ in inputs:
+                input_.append(1.0)
+        self.inputNodes = np.asarray(inputs)
+        # Fill in the hidden nodes
+        prevLayer = self.inputNodes
+        for i in range(len(self.hiddenNodes)):
+            self.hiddenNodes[i] = np.dot(
+                prevLayer, self.weights[i]
+            )
+            prevLayer = activationFunction(self.hiddenNodes[i])
+        # Fill in the output nodes
+        self.outputNodes = np.dot(
+            prevLayer, self.weights[-1]
+        )
+        # Return the activated output layer
+        return activationFunction(self.outputNodes)
+
+# Construct the network
+network = FeedforwardFunctionNetwork(
+    2, [3, 3], 1, bias=True, randomize=True
+)
+
+# Perform the feedforward function on a given set of inputs
+networkOutput = network.feedforward(
+    [
+        [-1, -1],
+        [-1, 1],
+        [1, -1],
+        [1, 1]
+    ],
+    sigmoid
+)
+
+# Print out the network's output
+print('\n\nThe network\'s output on the given input is:\n')
+print(networkOutput)
+print('\n')
---
+++ 5. Back-Propagation/utils/auxfunctions.py
@@ -0,0 +1,71 @@
+"""
+    Imported libraries
+"""
+
+import numpy as np
+from math import e
+
+
+"""
+    Aux Functions
+"""
+
+# Define sigmoid activation function
+@np.vectorize
+def sigmoid(x):
+    return 1.0 / (1.0 + e ** (-x))
+
+
+"""
+    Multi-Layer Network class 
+"""
+
+class MultiLayerNetwork:
+
+    def __init__(self, inputSize, hiddenSizes, outputSize,
+                 bias=True, randomize=True):
+        # If bias is set to true, we must add 1 to the input size
+        if bias:
+            inputSize += 1
+        self.bias = bias
+        # Create nodes of the network
+        self.createNodes(inputSize, hiddenSizes, outputSize)
+        # Create weights for the network
+        self.createWeights(inputSize, hiddenSizes, outputSize, randomize)
+
+    def createNodes(self, inputSize, hiddenSizes, outputSize):
+        # Create input nodes
+        self.inputNodes = np.zeros((inputSize))
+        # Create hidden nodes
+        self.hiddenNodes = []
+        for hiddenSize in hiddenSizes:
+            self.hiddenNodes.append(
+                np.zeros((hiddenSize))
+            )
+        # Create output nodes
+        self.outputNodes = np.zeros((outputSize))
+
+    def createWeights(self, inputSize, hiddenSizes, outputSize, randomize):
+        # Put all of the sizes into a single list for easy access
+        allSizes = hiddenSizes
+        allSizes.insert(0, inputSize)
+        allSizes.append(outputSize)
+        # Create all weight layers
+        self.weights = []
+        for i in range(1, len(allSizes)):
+            prevLayerSize = allSizes[i-1]
+            currentLayerSize = allSizes[i]
+            self.weights.append(
+                self.createWeightLayer(
+                    (prevLayerSize, currentLayerSize), 
+                    randomize
+                )
+            )
+
+    def createWeightLayer(self, size, randomize):
+        # Create a single weight layer given the size and whether the
+        # weights should be randomized
+        if randomize:
+            return np.random.rand(size[0], size[1]) * 2.0 - 1.0
+        else:
+            return np.zeros(size)
