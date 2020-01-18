13472bf203dead6e526682ff4bd36e7b84be01fb
jlehett <jlehett117@knights.ucf.edu>
2020-01-18 17:10:01 -0500

    Fixed some misnomers in the Section 6.1 file.

--- 6. Learning Rate and Momentum/6.1 Learning Rate/learningrate.py
+++ 6. Learning Rate and Momentum/6.1 Learning Rate/learningrate.py
@@ -21,32 +21,31 @@ from utils.auxfunctions import mapRange
 
 
 """
-    A 4/4/1 network was trained to learn the 4-bit parity problem
-    using plain batch back-propagation. Input values were +-1 and target
-    values were +-0.9. Tanh nonlinearities were used at the hidden and
-    output nodes. Initial weights were uniformly distributed in 
-    [-0.5, +0.5]. A single nonadaptive learning rate was used for the 
+    A demonstration of the effect of various learning rates and momentum
+    values on the time to converge for training neural networks.
+
+    Network was trained on the 2-bit parity problem. Input values were 
+    +- 1 and target values were +- 0.9. Tanh nonlinearities were used
+    at hidden and output nodes. Initial weights were uniformly distributed
+    in [-0.5, +0.5]. A single nonadaptive learning rate was used for the
     entire network.
 
-    Mean-Squared-Error was used as the error function. A variety of
-    learning rates from 0.001 to 10 (37 values) and momentums from 0
-    to 0.99 (14 values) were tested. One hundred trials were run for
-    each parameter pair. Each network was allowed 5000 training epochs.
-    Learning was considered successful (converged) if MSE < 0.001 or
-    if every pattern was classified correctly with error less than 0.2.
+    A variety of learning rates from 0.001 to 5.0 (50 values) and 
+    momentums from 0.0 to 0.99 (14 values) were tested. 10 trials were
+    run for each parameter pair, and each network was allowed 750
+    training epochs. Learning was considerd successful (converged) if
+    MSE < 0.001 or if every training pattern was classified correctly
+    with error less than 0.2.
 
-    If the change in MSE between epochs was less than 10^(-12), the 
-    network was considered stuck and training was halted early. The 
-    average convergence time for each parameter pair was calculated as 
-    the sum of the times for the converged networks divided by the number 
-    of networks that converged. If no networks converged for a particular
-    set of parameters, avg time was set to 5000 for graphing purposes.
-    The probability of convergence was estimated as the number of networks
-    that converged divided by the number trained (100).
+    The average convergence time for each parameter pair was calculated
+    as the sum of the times for the converged networks divided by the
+    number of networks that converged. The probability of convergence
+    was estimated as the number of networks that converged divided by
+    the number trained (10).
 """
 
-# Define the 4-bit parity problem
-def createAllBit4Strings():
+# Define the 2-bit parity problem
+def createAllBit2Strings():
     x, y = [], []
     maxValue = 2**2 - 1
     rangeValues = range(maxValue)
@@ -67,7 +66,7 @@ def createAllBit4Strings():
             y.append(-0.9)
     return np.asarray(x), np.asarray(y)
 
-x, y = createAllBit4Strings()
+x, y = createAllBit2Strings()
 
 # Define the neural network
 def createNetwork(lr, momentum):
