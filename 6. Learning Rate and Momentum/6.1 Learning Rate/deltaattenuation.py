e6c51f411dd8dfe2c0d1d361077c8f54198f811e
John Lehett <jlehett117@knights.ucf.edu>
2020-01-23 19:30:38 -0500

    Finished off Section 6.1 with a delta attenuation compensation example in PyTorch

--- 6. Learning Rate and Momentum/6.1 Learning Rate/deltaattenuation.py
+++ 6. Learning Rate and Momentum/6.1 Learning Rate/deltaattenuation.py
@@ -37,6 +37,10 @@ from utils.auxfunctions import mapRange
     output, and so on.
 """
 
+# Set the pytorch device to cuda if available
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(device)
+
 # Define the 4-bit parity problem
 def createAllBit4Strings():
     x, y = [], []
@@ -49,21 +53,21 @@ def createAllBit4Strings():
         for i in range(len(binaryStringArray)):
             binaryStringArray[i] = int(binaryStringArray[i])
             if binaryStringArray[i] == 0:
-                binaryStringArray[i] = -1
+                binaryStringArray[i] = 0
             if binaryStringArray[i] == 1:
                 evenOnes = not evenOnes
         x.append(binaryStringArray)
         if evenOnes:
-            y.append(0.9)
+            y.append(1.0)
         else:
-            y.append(-0.9)
+            y.append(0.0)
     return np.asarray(x), np.asarray(y)
 
 x, y = createAllBit4Strings()
-tensor_x = torch.Tensor(x)
-tensor_y = torch.Tensor(y)
+tensor_x = torch.from_numpy(x).float()
+tensor_y = torch.from_numpy(y).float()
 dataset = data.TensorDataset(tensor_x, tensor_y)
-dataloader = data.DataLoader(dataset)
+dataloader = data.DataLoader(dataset, batch_size=16, shuffle=True)
 
 # Create the Neural Network class via PyTorch
 class Network(nn.Module):
@@ -78,41 +82,68 @@ class Network(nn.Module):
 
     def forward(self, x):
         # define forward pass
-        x = F.sigmoid(self.fc1(x))
-        x = F.sigmoid(self.fc2(x))
-        x = F.sigmoid(self.fc3(x))
-        x = F.sigmoid(self.fc4(x))
+        x = torch.sigmoid(self.fc1(x))
+        x = torch.sigmoid(self.fc2(x))
+        x = torch.sigmoid(self.fc3(x))
+        x = torch.sigmoid(self.fc4(x))
         return x
 
 # Define the loss function
 criterion = nn.MSELoss()
 
 # Create the two networks
-normalNetwork = Network()
-compensatedNetwork = Network()
+normalNetwork = Network().to(device)
+compensatedNetwork = Network().to(device)
 
 # Define optimizers, one for normal learning rate, and one for delta
 # attenuation compensation
 import torch.optim as optim
 
-lr = 0.001
-normalOptimizer = optim.SGD(normalNetwork.parameters(), lr=lr, momentum=0.0)
+lr = 0.1
+normalOptimizer = optim.SGD(normalNetwork.parameters(), lr=lr, momentum=0.9)
 compensatedOptimizer = optim.SGD([
     {'params': compensatedNetwork.fc4.parameters(), 'lr': lr*6**0},
     {'params': compensatedNetwork.fc3.parameters(), 'lr': lr*6**1},
     {'params': compensatedNetwork.fc2.parameters(), 'lr': lr*6**2},
     {'params': compensatedNetwork.fc1.parameters(), 'lr': lr*6**3}
-], lr=lr, momentum=0.0)
+], lr=lr, momentum=0.9)
 
-"""
-    Testing
-"""
-
-for epoch in range(1000):
+# Train the two sets of networks
+normalNetwork.train()
+normalLosses = []
+for epoch in range(10000):
 
     running_loss = 0.0
     for i, data in enumerate(dataloader, 0):
         inputs, targets = data
+        inputs = inputs.to(device)
+        targets = targets.to(device)
+        targets = targets.view(-1, 1)
+
+        # zero the parameter gradients
+        normalOptimizer.zero_grad()
+
+        outputs = normalNetwork(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        normalOptimizer.step()
+
+        running_loss += loss.item()
+
+    normalLosses.append(running_loss)
+
+print('Finished Training Normal Network')
+
+compensatedNetwork.train()
+compensatedLosses = []
+for epoch in range(10000):
+
+    running_loss = 0.0
+    for i, data in enumerate(dataloader, 0):
+        inputs, targets = data
+        inputs = inputs.to(device)
+        targets = targets.to(device)
+        targets = targets.view(-1, 1)
 
         # zero the parameter gradients
         compensatedOptimizer.zero_grad()
@@ -123,7 +154,21 @@ for epoch in range(1000):
         compensatedOptimizer.step()
 
         running_loss += loss.item()
-    
-    print(running_loss / 16)
 
-print('Finished Training')
+    compensatedLosses.append(running_loss)
+
+print('Finished Training Compensated Network')
+
+# Graph the losses per epoch
+fig, ax = plt.subplots(1, 1)
+
+ax.plot(normalLosses, label='Normal Network')
+ax.plot(compensatedLosses, label='Compensated Network')
+
+ax.title.set_text('E(t) over Time\nα = 0.9')
+ax.set_xlabel('Epochs')
+ax.set_ylabel('E(t)\nMSE')
+
+ax.legend()
+
+plt.show()
